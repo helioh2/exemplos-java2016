48ede69b8d8cbbfacdbcb1e8e1a81ec9bbd111d0
Lenovo <Lenovo@200.236.4.234>
2016-09-30 20:48:12 -0300

    Diagrama trabalho 1

---
+++ src/Departamento.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Departamento {
+    
+    private String nome;
+//    private List<Escritorio> escritorios;
+//    private List<Departamento> departamentos;
+//    private List<Membro> membros;
+//    private Membro gerente;
+//    private Empresa empresa;
+    
+    
+}
